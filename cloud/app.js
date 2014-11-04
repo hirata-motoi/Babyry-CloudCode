0049a2b32f29203064df89fb96e8d49dba2c9e9d
kenji.suzuki <kenji.suzuki.1984@gmail.com>
2014-11-04 16:41:15 +0900

    add announce board

--- cloud/app.js
+++ cloud/app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var app = express();
 
 var email_verify = require("cloud/email_verify/check");
+var announce_board = require("cloud/announce_board/get");
 
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
 app.use(express.bodyParser());    // Middleware for reading request body
 
 app.get('/email_verify', email_verify.check);
+app.get('/announce_board', announce_board.get);
 
 app.listen();
