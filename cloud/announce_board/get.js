89c5642119357202f3ca08a3c2a700db6298a738
kenji.suzuki <kenji.suzuki.1984@gmail.com>
2014-11-10 21:07:06 +0900

    change logic of announce board

--- cloud/announce_board/get.js
+++ cloud/announce_board/get.js
@@ -1,4 +1,7 @@
 exports.get = function(req, res) {
+
+    var async = require('cloud/lib/async');
+
     // check of reqest parameter exist
     if(!req.query) {
         res.json({"error":"no_req_param"});
@@ -7,139 +10,191 @@ exports.get = function(req, res) {
         res.json({"error":"no_userid"});
     }
 
-    var user_data = new Array();
-    var child_data = new Array();
-    var childimage_data = new Array();
+    var userData = new Array();
+    var childData = new Array();
+    var childImageData = new Array();
+    var announceInfoData = {};
+    var announceInfoParamsData = {};
+    var announceInfoHistoryData = new Array();
 
-    var child_num = 0;
-    get_user_data();
-
-    function get_user_data() {
-        var user = new Parse.Query("_User");
-        user.equalTo("userId", req.query.userid);
-        user.find({
-            success: function(result) {
-                if (result.length == 0) {
-                    res.json({"error":"no_user_found"});
+    async.waterfall([
+        function(callback) {
+            var user = new Parse.Query("_User");
+            user.equalTo("userId", req.query.userid);
+            user.find({
+                success: function(result) {
+                    if (result.length == 0) {
+                        res.json({"error":"no_user_found"});
+                    }
+                    if (!result[0].get("familyId")) {
+                        res.json({"error":"cannot get familyId"});
+                    }
+                    userData = result;
+                    callback(null, result[0].get("familyId"));
+                },
+                error: function() {
+                    res.json({"error":"error_in_get_user_data"});
                 }
-                if (!result[0].get("familyId")) {
-                    res.json({"error":"cannot get familyId"});
+            });
+        },
+        function(familyId, callback) {
+            var child = new Parse.Query("Child");
+            child.equalTo("familyId", familyId);
+            child.find({
+                success: function(result) {
+                    if (result.length == 0) {
+                        res.json({"error":"no_child_found"});
+                    }
+                    childData = result;
+                    var childNum = result.length;
+                    for (i = 0; i < result.length; i++) {
+                        var classname = "ChildImage" + result[i].get("childImageShardIndex");
+                        var childObjectId = result[i].id;
+                        var childImage = new Parse.Query(classname);
+                        childImage.equalTo("imageOf", childObjectId);
+                        childImage.equalTo("bestFlag", "choosed");
+                        childImage.find({
+                            success: function(result) {
+                                childImageData.push(result);
+                                childNum--;
+                                if (childNum == 0) {
+                                    callback(null);
+                                }
+                            },
+                            error: function() {
+                                res.json({"error":"error_in_get_childimage_data"});
+                            }
+                        });
+                    }
+                },
+                error: function() {
+                    res.json({"error":"error_in_get_child_data"});
                 }
-                user_data = result;
-                get_child_data(result[0].get("familyId"));
-            },
-            error: function() {
-                res.json({"error":"error_in_get_user_data"});
-            }
-        });
-    }
-
-    function get_child_data(familyId) {
-        var child = new Parse.Query("Child");
-        child.equalTo("familyId", familyId);
-        child.find({
-            success: function(result) {
-                if (result.length == 0) {
-                    res.json({"error":"no_child_found"});
+            });
+        },
+        function(callback) {
+            var announceInfo = new Parse.Query("AnnounceInfo");
+            announceInfo.find({
+                success: function(result) {
+                    for (var i = 0; i < result.length; i++) {
+                        var key = result[i].get("key");
+                        var title = result[i].get("title");
+                        var message = result[i].get("message");
+                        announceInfoData[key] = {"title":title, "message":message};
+                    }
+                    callback(null);
+                },
+                error: function() {
+                    res.json({"error":"error_in_get_announce_info"});
                 }
-                child_num = result.length;
-                child_data = result;
-                for (i = 0; i < result.length; i++) {
-                    get_childimage_data("ChildImage" + result[i].get("childImageShardIndex"), result[i].id, i);
+            });
+        },
+        function(callback) {
+            var announceInfoParams = new Parse.Query("AnnounceInfoParams");
+            announceInfoParams.find({
+                success: function(result) {
+                    for (var i = 0; i < result.length; i++) {
+                        var key = result[i].get("key");
+                        var value = result[i].get("value");
+                        announceInfoParamsData[key] = value;
+                    }
+                    callback(null);
+                },
+                error: function() {
+                    res.json({"error":"error_in_get_announce_info_params"});
                 }
-            },
-            error: function() {
-                res.json({"error":"error_in_get_child_data"});
-            }
-        });
-    }
-
-    function get_childimage_data(classname, childObjectId, i) {
-        var childimage = new Parse.Query(classname);
-        childimage.equalTo("imageOf", childObjectId);
-        childimage.equalTo("bestFlag", "choosed");
-        childimage.find({
-            success: function(result) {
-                childimage_data[i] = result;
-                get_childimage_data_callback(result, childObjectId);
-            },
-            error: function() {
-                res.json({"error":"error_in_get_childimage_data"});
-            }
-        });
-    }
-
-    function get_childimage_data_callback(result, childObjectId) {
-        child_num--;
-        if (child_num < 1) {
-            get_announce_info();
+            });
+        },
+        function(callback) {
+            var announceInfoHistory = new Parse.Query("AnnounceInfoHistory");
+            announceInfoHistory.equalTo("userId", userData[0].get("userId"));
+            announceInfoHistory.find({
+                success: function(result) {
+                    for (var i = 0; i < result.length; i++) {
+                        var key = result[i].get("displayed");
+                        announceInfoHistoryData[key] = "1";
+                    }
+                    callback(null);
+                },
+                error: function() {
+                    res.json({"error":"error_in_get_announce_info_history_params"});
+                }
+            });
         }
-    }
+    ], function (err, result) {
+        if(err) {
+            res.json({"error":err});
+        }
 
-    function get_announce_info() {
+        var elapseDate = getElapseDate();
+        var activeRatio = getActiveRatio(elapseDate);
 
-        var elapsedDate = get_elapse_date();
-        var activity_ratio = get_activity(elapsedDate);
-        
-        // child birthday exist (at least one child)
-        var child_has_birthday = check_child_has_birthday();
+        var childHasBirthday = checkChildHasBirthday();
+        var userRegisterCompleted = checkUserRegisterCompleted();
 
-        // check user register complete
-        var user_register_completed = check_user_register_completed();
-
-        if (elapsedDate > 7 && activity_ratio > 0.8 && !child_has_birthday) {
-            return_response("childBirthday");
-        } else if (elapsedDate > 7 && activity_ratio > 0.8 && !user_register_completed) {
-            return_response("registerAccount");
+        if (elapseDate > announceInfoParamsData["interval"] && activeRatio > announceInfoParamsData["activeRatio"]) {
+            if (!childHasBirthday && !announceInfoHistoryData["childBirthday"]) {
+                returnResponse("childBirthday");
+            } else if (!userRegisterCompleted && !announceInfoHistoryData["registerAccount"]) {
+                returnResponse("registerAccount");
+            } else {
+                returnResponse("");
+            }
         } else {
-            res.json({"title":"","message":""});
+            returnResponse("");
+        }
+    });
+
+    function returnResponse(key) {
+        if (key == "") {
+            res.json({"error":"no_announce_info_found"});
+        } else {
+            var AnnounceInfoHistory = Parse.Object.extend("AnnounceInfoHistory");
+            var announceInfoHistory = new AnnounceInfoHistory();
+            announceInfoHistory.set("userId", userData[0].get("userId"));
+            announceInfoHistory.set("displayed", key);
+            announceInfoHistory.save(null, {
+                success: function(result) {
+                    res.json({"key":key,"title":announceInfoData[key]["title"],"message":announceInfoData[key]["message"]});
+                },
+                error: function(gameScore, error) {
+                    res.json({"error":"cannot_save_info_history"});
+                }
+            });
         }
     }
 
-    function return_response(key) {
-        var announceInfo = new Parse.Query("AnnounceInfo");
-        announceInfo.equalTo("key", key);
-        announceInfo.find({
-            success: function(result) {
-                res.json({"title":result[0].get("title"),"message":result[0].get("message")});
-            },
-            error: function() {
-                res.json({"error":"error_in_get_announce_info"});
-            }
-        });
-    }
-
-    function get_elapse_date() {
-        var createdAt = user_data[0]["createdAt"];
+    function getElapseDate() {
+        var createdAt = userData[0]["createdAt"];
         var now = new Date();
         var diff = now.getTime() - createdAt.getTime();
         return Math.floor(diff/(24*60*60*1000));
     }
 
-    function get_activity(elapsedDate) {
+    function getActiveRatio(elapsedDate) {
         if (elapsedDate == 0) {
             return 0;
         }
-        var max_length = 0;
-        for (i = 0; i < childimage_data.length; i++) {
-            if (max_length < childimage_data[i].length) {
-                max_length = childimage_data[i].length;
+        var maxLength = 0;
+        for (i = 0; i < childImageData.length; i++) {
+            if (maxLength < childImageData[i].length) {
+                maxLength = childImageData[i].length;
             }
         }
-        return max_length/elapsedDate;
+        return maxLength/elapsedDate;
     }
 
-    function check_child_has_birthday() {
-        for (i = 0; i < child_data.length; i++) {
-            if (child_data[i].get("birthday")) {
+    function checkChildHasBirthday() {
+        for (i = 0; i < childData.length; i++) {
+            if (childData[i].get("birthday")) {
                 return Boolean(true);
             }
         }
         return Boolean(false);
     }
 
-    function check_user_register_completed() {
-        var username = user_data[0].get("username");
+    function checkUserRegisterCompleted() {
+        var username = userData[0].get("username");
         if (username.length == 8 && username.indexOf("@") == -1) {
             return Boolean(false);
         }
