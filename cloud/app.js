621a0f2647ca2dfbf0b6d3a53ab001405d350118
hirata.motoi <hirata.motoi@gmail.com>
2014-11-20 14:52:20 +0900

    fix

--- cloud/app.js
+++ cloud/app.js
@@ -3,7 +3,7 @@ var app = express();
 
 var email_verify = require("cloud/email_verify/check");
 var announce_board = require("cloud/announce_board/get");
-var announce_board = require("cloud/user_disable/disable");
+var user_disable = require("cloud/user_disable/disable");
 
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
