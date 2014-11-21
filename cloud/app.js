5b6d5b5a8e24c0a74c6e43ddabdd4a89c61b86af
hirata.motoi <hirata.motoi@gmail.com>
2014-11-21 17:48:42 +0900

    Added config.js and move user_delete from main.js

--- cloud/app.js
+++ cloud/app.js
@@ -3,6 +3,9 @@ var app = express();
 
 var email_verify = require("cloud/email_verify/check");
 var announce_board = require("cloud/announce_board/get");
+var user_delete = require("cloud/user_delete/delete");
+var config_loader = require("cloud/config");
+var config = config_loader.get();
 
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
@@ -10,5 +13,6 @@ app.use(express.bodyParser());    // Middleware for reading request body
 
 app.get('/email_verify', email_verify.check);
 app.get('/announce_board', announce_board.get);
+app.post('/user_delete', express.basicAuth(config["basic_auth_name"], config["basic_auth_password"]), user_delete.execute_delete);
 
 app.listen();
