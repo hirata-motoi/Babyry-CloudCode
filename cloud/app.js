b9aab212168ea5a6882bf7f9711064908d6dcb4f
kenji.suzuki <kenji.suzuki.1984@gmail.com>
2015-01-28 18:05:22 +0900

    add comment get method

--- cloud/app.js
+++ cloud/app.js
@@ -4,6 +4,7 @@ var app = express();
 var email_verify = require("cloud/email_verify/check");
 var announce_board = require("cloud/announce_board/get");
 var user_delete = require("cloud/user_delete/delete");
+var comment_get = require("cloud/comment/get");
 var config_loader = require("cloud/config");
 var config = config_loader.get();
 
@@ -13,6 +14,7 @@ app.use(express.bodyParser());    // Middleware for reading request body
 
 app.get('/email_verify', email_verify.check);
 app.get('/announce_board', announce_board.get);
+app.get('/comment_get', comment_get.get);
 app.post('/user_delete', express.basicAuth(config["basic_auth_name"], config["basic_auth_password"]), user_delete.execute_delete);
 
 app.listen();
